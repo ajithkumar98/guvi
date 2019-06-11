e04144f90b627528745a1ad8b28a16af21f74f4d
Ajithkumar <ajithbmwkumar@gmail.com>
2019-06-11 10:51:34 +0530

    Update repeat.py

--- repeat.py
+++ repeat.py
@@ -1,6 +1,10 @@
 l=int(input())
 arr=list(map(int,input().split()))
+flag=0
 for i in range(0,len(arr)):
     if(arr.count(arr[i])>1):
         print(arr[i])
+        flag=1
         break
+if flag==0:
+    print("unique")
