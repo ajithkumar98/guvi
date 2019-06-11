d0facc9ae6ade7576a933cc22a834daab4afd456
Ajithkumar <ajithbmwkumar@gmail.com>
2019-06-11 09:10:34 +0530

    Update prob.c

--- prob.c
+++ prob.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	long long int num;
+	scanf("%lld",&num);
+	if(num>0){
+		printf("Positive");
+	}
+	else if(num<0){
+		printf("Negative");
+	}
+	else{
+		printf("Zero");
+	}
+}
 
