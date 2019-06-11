606bd5596f7706a9dec837641810bbce8bb864e3
Ajithkumar <ajithbmwkumar@gmail.com>
2019-06-11 14:05:18 +0530

    Update find.py

--- find.py
+++ find.py
@@ -1,30 +1,12 @@
-
-def subArraySum(arr, n, sum): 
-	
-	curr_sum = arr[0] 
-	start = 0
-
-	i = 1
-	while i <= n: 
-		 
-		while curr_sum > sum and start < i-1: 
-		
-			curr_sum = curr_sum - arr[start] 
-			start += 1
-			
-		if curr_sum == sum: 
-			print ("YES") 
-			return 1
-
-		if i < n: 
-			curr_sum = curr_sum + arr[i] 
-		i += 1
- 
-	print ("NO") 
-	return 0
-
-
+flag=0
 n,m=map(int,input().split())
 arr=list(map(int,input().split()))
-subArraySum(arr, n, m) 
+for i in range(0,len(arr)):
+    for j in range(i+1,len(arr)):
+        if(arr[i]+arr[j]==m):
+            print("YES")
+            flag=1
+
+if(flag==0):
+    print("NO")
 
