8213900fbe17ddbe66fcb9dd7bb4ebab01abb84e
Ajithkumar <ajithbmwkumar@gmail.com>
2019-06-11 14:07:24 +0530

    Update find.py

--- find.py
+++ find.py
@@ -6,6 +6,9 @@ for i in range(0,len(arr)):
         if(arr[i]+arr[j]==m):
             print("YES")
             flag=1
+            break
+    if(flag==1):
+        break
 
 if(flag==0):
     print("NO")
