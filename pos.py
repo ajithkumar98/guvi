3eaf8dbaf87b8e03350f7cc7df96e99816a10c70
Ajithkumar <ajithbmwkumar@gmail.com>
2019-06-11 09:25:08 +0530

    Update pos.py

--- pos.py
+++ pos.py
@@ -1,1 +1,9 @@
-
+#code
+no=(input())
+if(isinstance(no,str)):
+    if(no=='a' or no=='e' or no=='i' or no=='o' or no=='u'):
+        print("Vowel")
+    else:
+        print("Invalid")
+else:
+    print("Invalid")
